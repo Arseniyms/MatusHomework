5ca5fa72a359d2ff5acc166884cb002cdf92e283
Arseniy <arsmatus@list.ru>
2022-12-02 14:23:11 +0300

    0.0.14

--- MatusHomework/MatusHomework.swift
+++ MatusHomework/MatusHomework.swift
@@ -26,7 +26,7 @@ public class SquareView: UIView {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     
